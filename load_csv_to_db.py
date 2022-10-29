706e82f7fff06df1a33dadc6264a4d1b6f18b2da
korsakov.iyu <korsakov.iyu@dns-shop.ru>
2022-10-29 20:27:19 +0600

    Изменен проект dag_hurricanes_and_typhoons: - Добавлены скрипты в README.MD - Добавлен python скрипт с загрузкой данных в БД - Добавлен список пакет для установки - Изменен .gitignore - Добавлены .csv исходники

---
+++ load_csv_to_db.py
@@ -0,0 +1,51 @@
+import argparse
+import pandas as pd
+from sqlalchemy import create_engine
+
+def load_data_to_db(path_to_file: str):
+    """
+
+    :param path_to_file:
+    :return:
+    """
+
+    df = pd.read_csv(path_to_file)
+
+    engine_str = f'postgresql://postgres:postgres@localhost:1/postgres'
+    engine = create_engine(engine_str)
+
+    new_name_columns = df.columns
+    new_name_columns = new_name_columns.map(lambda x: x.lower())
+    new_name_columns = new_name_columns.map(lambda x: x.replace(' ', '_'))
+
+    df.columns = new_name_columns
+
+    df.to_sql(
+        con=engine,
+        chunksize=10024,
+        index=False,
+        if_exists='append',
+        name='cyclones',
+        schema='public',
+        method='multi',
+    )
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "-p",
+        help="Путь к файлу",
+    )
+
+    args = vars(ap.parse_args())
+
+    try:
+        load_data_to_db(path_to_file=args['p'])
+    except ValueError:
+        print('Укажите путь к файлу')
+    except SyntaxError:
+        print('Путь к файлу, должен быть в str формате')
+    except FileNotFoundError:
+        print('Неверный путь к файлу или наименование')
+    except Exception as ex:
+        print('Посмотрите документацию, через команду `python load_csv_to_db.py -h`')
